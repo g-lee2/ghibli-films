ea73d4ce46f4328e66ba13b45ed075d467055783
Grace Lee <gracexsnoopy@gmail.com>
2022-11-03 05:15:00 -0700

    Fix UI logic to display one movie and display things like title and director

--- src/index.js
+++ src/index.js
@@ -2,48 +2,56 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import GetFilms from './js/film';
-import GetPeople from './js/film';
+//import GetPeople from './js/film';
 
-function getFilms() {
-  const response = GetFilms.getInfo();
-  if (response == 200) {
-    printFilmElements(response.title);
+async function getFilms() {
+  const response = await GetFilms.getInfo();
+  if (response) {
+    printFilmElements(response);
   } else {
     printFilmError(response);
   }
 }
 
-function getPeople() {
-  const response = GetPeople.getInfo();
-  if (response == 200) {
-    printPeopleElements(response);
-  } else {
-    printPeopleError(response);
-  }
-}
+// function getPeople() {
+//   const response = GetPeople.getInfo();
+//   if (response == 200) {
+//     printPeopleElements(response);
+//   } else {
+//     printPeopleError(response);
+//   }
+// }
 
-
-function printFilmElements(response) {
-  document.querySelector('.result-films').innerText = response;
+function printFilmElements(data) {
+  document.getElementById('film-title').innerHTML = data.title; 
+  document.getElementById('film-original-title').innerHTML = data.original_title;
+  document.getElementById('film-romanised-title').innerHTML = data.original_title_romanised;
+  document.getElementById('film-image').innerHTML = data.image;
+  document.getElementById('film-description').innerHTML = data.description;
+  document.getElementById('film-director').innerHTML = data.director;
+  document.getElementById('film-producer').innerHTML = data.producer;
+  document.getElementById('film-release-date').innerHTML = data.release_date;
+  document.getElementById('film-running-time').innerHTML = data.running_time;
 }
 
 function printFilmError(error) {
-  document.querySelector('.result-films').innerText = error;
+  document.getElementsByClassName('film-title').innerText = error;
 }
 
-function printPeopleElements(response) {
-  document.querySelector('.result-people').innerText = response;
-}
+// function printPeopleElements(data) {
+//   document.querySelector('.result-people').innerText = response;
+// }
 
-function printPeopleError(error) {
-  document.querySelector('.result-people').innerText = error;
-}
+// function printPeopleError(error) {
+//   document.querySelector('.result-people').innerText = error;
+// }
 
-function handleFormSubmission() {
+function handleFormSubmission(event) {
+  event.preventDefault();
   getFilms();
-  getPeople();
+  // getPeople();
 }
 
 window.addEventListener("load", function() {
-  handleFormSubmission();
+  document.querySelector("form").addEventListener("submit", handleFormSubmission);
 });
