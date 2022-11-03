2c3a17307ccd60b597a1b624e35fb930a455c2e0
Grace Lee <gracexsnoopy@gmail.com>
2022-11-03 05:14:16 -0700

    First get one movie to go through and display

--- src/js/film.js
+++ src/js/film.js
@@ -8,34 +8,37 @@
 // https://ghibliapi.herokuapp.com/species/<id>
 // https://ghibliapi.herokuapp.com/vehicles
 // https://ghibliapi.herokuapp.com/vehicles/<id>
-const baseURL = 'https://ghibliapi.herokuapp.com/';
+//const baseURL = 'https://ghibliapi.herokuapp.com/';
 
 export default class GetFilms {
-  static getInfo() {
-    const filmRequestEndpoint = 'films';
-    const urlToFetch = `${baseURL}${filmRequestEndpoint}`;
-    fetch(urlToFetch)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        return data;
-      });
+  static async getInfo() {
+    try {
+      //const filmRequestEndpoint = 'films';
+      const response = await fetch ('https://ghibliapi.herokuapp.com/films');
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusTest} ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      console.log(jsonifiedResponse[0]);
+      return jsonifiedResponse[0];
+    } catch(error) {
+      return error;
+    }
   }
 }
 
-export class GetPeople {
-  static getInfo() {
-    const filmRequestEndpoint = 'people';
-    const urlToFetch = `${baseURL}${filmRequestEndpoint}`;
-    fetch(urlToFetch)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        return data;
-      });
-  }
-}
+// export class GetPeople {
+//   static getInfo() {
+//     const filmRequestEndpoint = 'people';
+//     const urlToFetch = `${baseURL}${filmRequestEndpoint}`;
+//     fetch(urlToFetch)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//         return data;
+//       });
+//   }
+// }
